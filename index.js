73b79bb8ba60b8b08e8e4ffd0e0010f707c15536
Dusan Vostic <vosticdusan@gmail.com>
2021-02-03 10:32:30 +0100

    JS variable scope task from Scrum.

---
+++ index.js
@@ -0,0 +1,29 @@
+// const forLoopWithVarVariable = () => {
+//   for (let i = 0; i < 10; i++) {
+//     var x = i;
+//   }
+//   console.log(x);
+// }
+// forLoopWithVarVariable();
+
+
+// const forLoopWithLetVariable = () => {
+//   for (let i = 0; i < 10; i++) {
+//     let x = i;
+//   }
+//   console.log(x);
+// }
+// forLoopWithLetVariable();
+
+
+// const x = 5;
+// function printVariableOutsideOfFunction () {
+//   console.log(x);
+// }
+// printVariableOutsideOfFunction();
+
+
+function createVariable() {
+  const x = 3;
+}
+console.log(x);
